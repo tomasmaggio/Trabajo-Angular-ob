9cf3c2789027ad20f4c667a7558e004e267be7bb
tomasmaggio <tomasmaggio220@gmail.com>
2023-06-29 20:16:40 -0300

    Cree el componente 'item'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { ItemsComponent } from './component/items/items.component';
+import { ItemComponent } from './component/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ItemsComponent,
+    ItemComponent,
 
   ],
   imports: [

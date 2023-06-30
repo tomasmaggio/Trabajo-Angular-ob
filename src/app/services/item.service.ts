2919ccb6cf22cd07930ff651c1f244726401518d
tomasmaggio <tomasmaggio220@gmail.com>
2023-06-30 02:32:07 -0300

    Estilos terminados

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -17,12 +17,40 @@ export class ItemService {
       tittle: 'Papa frita Lays',
       price: 3.5,
       quantity: 2,
-      completed: true
+      completed: false
     },
     {
       id: 2,
       tittle: 'Coca-Cola',
       price: 8,
+      quantity: 1,
+      completed: false
+    },
+    {
+      id: 3,
+      tittle: 'Pancho',
+      price: 3,
+      quantity: 18,
+      completed: true
+    },
+    {
+      id: 4,
+      tittle: 'Leche',
+      price: 20,
+      quantity: 2,
+      completed: true
+    },
+    {
+      id: 5,
+      tittle: 'Un perrito',
+      price: 250,
+      quantity: 1,
+      completed: true
+    },
+    {
+      id: 6,
+      tittle: 'Juego de palos de golf',
+      price: 550,
       quantity: 1,
       completed: false
     }
